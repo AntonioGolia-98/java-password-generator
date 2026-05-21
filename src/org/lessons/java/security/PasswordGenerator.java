48914176f5a99b2d4b82de56a24da3ca7dda4d4f
AntonioGolia-98 <antoniog98@icloud.com>
2026-05-21 18:21:19 +0200

    creazioni variabili

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,5 +1,14 @@
 package org.lessons.java.security;
 
 public class PasswordGenerator {
+    public static void main(String[] args) {
 
+        String nome;
+        String cognome;
+        String colorePreferito;
+        int giorno;
+        int mese;
+        int anno;
+
+    }
 }
